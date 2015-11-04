910c322ff8daa04a822bd7705044eee403f12282
abautist <auggie.bautista@gmail.com>
2015-11-03 18:18:54 -0800

    some styling

--- controllers/payments.js
+++ controllers/payments.js
@@ -31,47 +31,47 @@ router.post("/completed", function(req, res) {
 	var stripeToken = req.body.stripeToken;
 	var stripeEmail = req.body.stripeEmail;
 	var userId = req.currentUser.id;
-//stripe charge
-	var charge = {
-		amount: 3000,
-		currency: "usd",
-		card: stripeToken
-		};
-	
-	for (var key in req.body) {
-		console.log(key+": "+req.body[key]);
-	}
-	
 
-	stripe.charges.create(charge, 
-		function(err, charge) {
-			if (err && err.type === 'StripeCardError') {
-	    		console.log("Error Error");
-	  		} else {
-	  			console.log('Successful charge sent to Stripe!');
-	  			console.log(stripeToken);
-	  			
-	  			db.cart.destroy({
-	  				where: {
-	  					userId: userId
-	  				}
-	  			}).then(function() {
-					console.log("cart cleared");
-					req.currentUser.createSale({
-						email: stripeEmail,
-						price: charge.amount,
-						stripeToken: stripeToken
-					}).then(function(sale) {
-						console.log(sale.get());
-						res.render("payments/show");
-					});
-				}).catch(function() {
-					console.log("error")
-				});
-				
-				
-			}
-		});
+//stripe charge
+	db.cart.findById(userId).then(function(cart) {
+		var charge = {
+			amount: cart.price,
+			currency: "usd",
+			card: stripeToken
+			};
+		
+		for (var key in req.body) {
+			console.log(key+": "+req.body[key]);
+		}
+		
+		stripe.charges.create(charge, 
+			function(err, charge) {
+				if (err && err.type === 'StripeCardError') {
+		    		console.log("Error Error");
+		  		} else {
+		  			console.log('Successful charge sent to Stripe!');
+		  			console.log(stripeToken);
+		  			
+		  			db.cart.destroy({
+		  				where: {
+		  					userId: userId
+		  				}
+		  			}).then(function() {
+						console.log("cart cleared");
+						req.currentUser.createSale({
+							email: stripeEmail,
+							price: charge.amount,
+							stripeToken: stripeToken
+						}).then(function(sale) {
+							console.log(sale.get());
+							res.render("payments/show");
+						});
+					}).catch(function() {
+						console.log("error")
+					});			
+				}
+			});
+	});
 });
 
 			
